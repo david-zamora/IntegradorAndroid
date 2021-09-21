71dfc762d114b37878a9c8662e650ebfe01b7a31
david-zamora <david.zamora@mercadolibre.com.co>
2021-09-21 15:07:43 -0500

    MainAtivity and boton start

--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.notbored
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+       /* val inputText = findViewById<EditText>(R.id.txt_number)
+        inputText.addTextChangedListener{ }
+        //revisar setOnFocusChangeListener
+
+        var number: Int = Integer.parseInt(inputText.text.toString())
+
+        */
+
+        val actionButtonStart = findViewById<Button>(R.id.btn_Start)
+
+        actionButtonStart.setOnClickListener {
+                    val intent : Intent = Intent(this, Activities :: class.java)
+                    startActivity(intent)
+
+                }
+        }
     }
-}
+
+
+
