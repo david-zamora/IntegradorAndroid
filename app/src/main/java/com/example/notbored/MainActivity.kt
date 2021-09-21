b244843e44b449862c5418205b1de8e9faa4be2e
EzequielML <ezequiel.aliotta@mercadolibre.com>
2021-09-21 17:07:32 -0300

    Cambios realizados 21 sep 17 07hs

--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.notbored
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_goTerms = findViewById<Button>(R.id.btn_goTerms)
+        val btn_goActivities = findViewById<Button>(R.id.btn_activities)
+
+        btn_goTerms.setOnClickListener {
+            val intent : Intent = Intent(this, TermsAndConditions::class.java)
+            startActivity(intent)
+        }
+
+
+        btn_goActivities.setOnClickListener {
+            val intent : Intent = Intent(this, Activities::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/notbored/SugerenceScreen.kt
@@ -0,0 +1,11 @@
+package com.example.notbored
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SugerenceScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sugerence_screen)
+    }
+}
