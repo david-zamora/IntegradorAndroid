7078878bee5b92d56ddc34400a97e854cfb1e45c
EzequielML <ezequiel.aliotta@mercadolibre.com>
2021-09-22 15:44:50 -0300

    cambios realizados a sugerencias y activities

--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.notbored
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
 import org.w3c.dom.Text
@@ -18,8 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         val actionButtonStart = findViewById<Button>(R.id.btn_Start)
 
+        val tv_terms = findViewById<TextView>(R.id.tv_terms)
+
+        var sharedPrefs = getSharedPreferences("prefs" , MODE_PRIVATE)
+
+        val et_participants = findViewById<EditText>(R.id.txt_number)
+
+        val numberOfParticipants = et_participants.text.toString().toInt()
+
+
         actionButtonStart.setOnClickListener {
-                    val intent : Intent = Intent(this, Activities :: class.java)
+
+            val intent : Intent = Intent(this, Activities :: class.java)
                     startActivity(intent)
 
                 }
--- app/src/main/java/com/example/notbored/SugerenceScreen.kt
+++ app/src/main/java/com/example/notbored/SugerenceScreen.kt
@@ -1,11 +1,57 @@
 package com.example.notbored
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 
 class SugerenceScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sugerence_screen)
+
+        val randomTitle = intent.getStringExtra("RandomSugerence")
+       val title = intent.getStringExtra("titleSugerence")
+
+        var prize = 0.5
+
+        val tv_titleSugerence = findViewById<TextView>(R.id.tv_TitleSugerence)
+        val tv_prize = findViewById<TextView>(R.id.tv_prize)
+        val tv_participants = findViewById<TextView>(R.id.tv_participants)
+        val btn_tryAnother = findViewById<Button>(R.id.btn_tryAnother)
+        val ibtn_toActivities = findViewById<ImageButton>(R.id.ibtn_toActivityMenu)
+
+
+        if (intent.hasExtra("titleSugerence")){
+            tv_titleSugerence.text = title
+        } else {
+            tv_titleSugerence.text = randomTitle
+        }
+
+        if (prize == 0.0) {
+            tv_prize.text = "Free"
+        } else if(prize > 0.0 && prize < 0.3) {
+            tv_prize.text = "Low"
+        } else if (prize > 0.3 && prize < 0.6) {
+            tv_prize.text = "Medium"
+        } else {
+            tv_prize.text = "High"
+        }
+
+
+        ibtn_toActivities.setOnClickListener {
+            val intent = Intent(this, Activities::class.java)
+            startActivity(intent)
+        }
+        btn_tryAnother.setOnClickListener {
+
+        }
+
+
+
+
     }
 }
